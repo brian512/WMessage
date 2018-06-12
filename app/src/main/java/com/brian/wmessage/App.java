874585884b314c06caa1225ff57aaa45de21ce1e
huamanman <huamanman@xunlei.com>
2018-06-12 16:01:58 +0800

    添加log开关

--- app/src/main/java/com/brian/wmessage/App.java
+++ app/src/main/java/com/brian/wmessage/App.java
@@ -1,6 +1,7 @@
 package com.brian.wmessage;
 
 import com.brian.common.base.BaseApplication;
+import com.brian.common.utils.LogUtil;
 import com.brian.wmessage.imservice.IMServiceManager;
 
 /**
@@ -11,6 +12,7 @@ public class App extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        LogUtil.mIsDebugMode = BuildConfig.DEBUG_ENABLE;
         if (isMainProcess()) {
             IMServiceManager.getInstance().init(this);
         }
